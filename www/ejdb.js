cdc96fecd0700952ec8695426bd251c49cb80770
Jason Thomas <jthomas@silvervue.com>
2016-02-26 08:28:04 -0700

    Javascript interface

--- www/ejdb.js
+++ www/ejdb.js
@@ -1,7 +1,21 @@
 /*global cordova, module*/
 
 module.exports = {
-    init: function (name, successCallback, errorCallback) {
-	cordova.exec(successCallback, errorCallback, "EJDB", "init", [name]);
+    createDatabaseWithPath: function (path, successCallback, errorCallback) {
+	cordova.exec(successCallback, errorCallback, "EJDBPlugin", "init", [path]);
+    },
+    initializeCollectionWithName: function(name, successCallback, errorCallback) {
+	cordova.exec(successCallback, errorCallback, "EJDBPlugin", "initializeCollectionWithName", [name]);	
+    },
+    saveObject: function(name, someObject, successCallback, errorCallback) {
+	var jsonObj = JSON.stringify(someObject);
+	cordova.exec(successCallback, errorCallback, "EJDBPlugin", "initializeCollectionWithName", [name, jsonObj]);	
+    },
+    find: function(name, query, successCallback, errorCallback) {
+	var jsonQuery = JSON.stringify(someQuery);
+	cordova.exec(successCallback, errorCallback, "EJDBPlugin", "find", [name, jsonQuery]);		
+    },
+    remove: function(name, uid, successCallback, errorCallback) {
+	cordova.exec(successCallback, errorCallback, "EJDBPlugin", "remove", [name, uid]);	
     }
 };
