6ce0f5f31ec281fb35a7616deec42d7fc0f36fcb
Jason Thomas <jthomas@silvervue.com>
2016-03-02 15:23:11 -0700

    Blunt instrument. Synchronize everything.

--- src/ios/EJDBPlugin.m
+++ src/ios/EJDBPlugin.m
@@ -8,172 +8,185 @@ static EJDBDatabase *jb = nil;
 
 static NSMutableDictionary *collectionHandles = nil;
 
-- (CDVPluginResult*) createDatabaseWithPath:(CDVInvokedUrlCommand*)command {
-    NSString* callbackId = [command callbackId];
 
-    NSString* path = [[command arguments] objectAtIndex:0];
-    
-    if(jb && [jb isOpen]) {
-        [jb close];
+- (CDVPluginResult*) createDatabaseWithPath:(CDVInvokedUrlCommand*)command {
+
+    @synchronized(self) {
+        NSString* callbackId = [command callbackId];
+        
+        NSString* path = [[command arguments] objectAtIndex:0];
+        
+        if(jb && [jb isOpen]) {
+            [jb close];
+        }
+        
+        // Initialize collection handles.
+        collectionHandles = [[NSMutableDictionary alloc] init];
+        
+        
+        BOOL opened = false;
+        // All data will be saved to silversearch.db file.
+        jb = [[EJDBDatabase alloc] initWithPath: path dbFileName:@"silversearch.db"];
+        
+        if(jb) {
+            opened = [jb openWithError: NULL];
+        }
+        CDVPluginResult* result = [CDVPluginResult
+                  resultWithStatus:CDVCommandStatus_ERROR];
+        
+        if (jb && opened) {
+            result = [CDVPluginResult
+                      resultWithStatus:CDVCommandStatus_OK];
+            [self success:result callbackId:callbackId];
+        }
+        else {
+            [self error:result callbackId:callbackId];
+        }
+        
+        return result;
     }
     
-    // Initialize collection handles.
-    collectionHandles = [[NSMutableDictionary alloc] init];
-    
-    
-    BOOL opened = false;
-    // All data will be saved to silversearch.db file.
-    jb = [[EJDBDatabase alloc] initWithPath: path dbFileName:@"silversearch.db"];
-    
-    if(jb) {
-        opened = [jb openWithError: NULL];
-    }
-    CDVPluginResult* result = [CDVPluginResult
-              resultWithStatus:CDVCommandStatus_ERROR];
-    
-    if (jb && opened) {
-        result = [CDVPluginResult
-                  resultWithStatus:CDVCommandStatus_OK];
-        [self success:result callbackId:callbackId];
-    }
-    else {
-        [self error:result callbackId:callbackId];
-    }
-    
-    return result;
 }
 
 - (CDVPluginResult*) initializeCollectionWithName:(CDVInvokedUrlCommand*)command {
-    NSString* callbackId = [command callbackId];
-    
-    NSString* name = [[command arguments] objectAtIndex:0];
+    @synchronized(self) {
+        NSString* callbackId = [command callbackId];
+        
+        NSString* name = [[command arguments] objectAtIndex:0];
 
-    EJDBCollection *collection = [[EJDBCollection alloc]initWithName:name db:jb];
-    
-    // This creates the collection for you if it doesn't already exist
-    [collection openWithError:NULL];
-    
-    //Already have one that you want to retrieve?
-    collection = [jb ensureCollectionWithName:name error: NULL];
+        EJDBCollection *collection = [[EJDBCollection alloc]initWithName:name db:jb];
+        
+        // This creates the collection for you if it doesn't already exist
+        [collection openWithError:NULL];
+        
+        //Already have one that you want to retrieve?
+        collection = [jb ensureCollectionWithName:name error: NULL];
 
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_ERROR];
+        CDVPluginResult* result = [CDVPluginResult
+                                   resultWithStatus:CDVCommandStatus_ERROR];
 
-    if ((collection)) {
-    
-        // Save collection instance by name.
-        [collectionHandles setObject: collection forKey: name];
+        if ((collection)) {
+        
+            // Save collection instance by name.
+            [collectionHandles setObject: collection forKey: name];
 
-        result = [CDVPluginResult
-                  resultWithStatus:CDVCommandStatus_OK];
-        [self success:result callbackId:callbackId];
+            result = [CDVPluginResult
+                      resultWithStatus:CDVCommandStatus_OK];
+            [self success:result callbackId:callbackId];
+        }
+        else {
+            [self error:result callbackId:callbackId];
+        }
+        
+        return result;
     }
-    else {
-        [self error:result callbackId:callbackId];
-    }
-    
-    return result;
 }
 
 - (CDVPluginResult*) saveObject:(CDVInvokedUrlCommand*)command {
-    NSString* callbackId = [command callbackId];
-    
-    NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* json = [[command arguments] objectAtIndex:1];
-    
-    // Find collection by the name handle.
-    EJDBCollection *collection = (EJDBCollection*)[collectionHandles objectForKey: name];
-    
-    NSError* error;
-    NSData* objectData = [json dataUsingEncoding:NSUTF8StringEncoding];
-    NSDictionary* jsonDict = [NSJSONSerialization JSONObjectWithData:objectData options:kNilOptions error:&error];
-    
-    
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_ERROR];
-    
-    if (collection && objectData && jsonDict && [collection saveObject: jsonDict]) {
+    @synchronized(self) {
+        NSString* callbackId = [command callbackId];
         
-        result = [CDVPluginResult
-                  resultWithStatus:CDVCommandStatus_OK];
-        [self success:result callbackId:callbackId];
+        NSString* name = [[command arguments] objectAtIndex:0];
+        NSString* json = [[command arguments] objectAtIndex:1];
+        
+        // Find collection by the name handle.
+        EJDBCollection *collection = (EJDBCollection*)[collectionHandles objectForKey: name];
+        
+        NSError* error;
+        NSData* objectData = [json dataUsingEncoding:NSUTF8StringEncoding];
+        NSDictionary* jsonDict = [NSJSONSerialization JSONObjectWithData:objectData options:kNilOptions error:&error];
+        
+        
+        CDVPluginResult* result = [CDVPluginResult
+                                   resultWithStatus:CDVCommandStatus_ERROR];
+        
+        if (collection && objectData && jsonDict && [collection saveObject: jsonDict]) {
+            
+            result = [CDVPluginResult
+                      resultWithStatus:CDVCommandStatus_OK];
+            [self success:result callbackId:callbackId];
+        }
+        else {
+            [self error:result callbackId:callbackId];
+        }
+        
+        
+        return result;
     }
-    else {
-        [self error:result callbackId:callbackId];
-    }
-    
-    
-    return result;
 }
 
 - (CDVPluginResult*) find:(CDVInvokedUrlCommand*)command {
-    NSString* callbackId = [command callbackId];
-    
-    NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* query = [[command arguments] objectAtIndex:1];
-    
-    // Find collection by the name handle.
-    EJDBCollection *collection = (EJDBCollection*)[collectionHandles objectForKey: name];
-    
-    NSError* error;
-    NSData* objectData = [query dataUsingEncoding:NSUTF8StringEncoding];
-    NSDictionary* jsonDict = [NSJSONSerialization JSONObjectWithData:objectData options:kNilOptions error:&error];
-    
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_ERROR];
-    
-    if (collection && objectData && jsonDict) {
-        EJDBQuery *query = [[EJDBQuery alloc]initWithCollection:collection
-                                                          query:jsonDict];
-
-        NSArray* results = [query fetchObjects];
-        NSMutableDictionary *postDict = [[NSMutableDictionary alloc]init];
-        [postDict setValue:results forKey:@"results"];
-        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:postDict options:0 error:nil];
-        NSString* jsonStr = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    @synchronized(self) {
+        NSString* callbackId = [command callbackId];
         
-        if(!jsonData) {
+        NSString* name = [[command arguments] objectAtIndex:0];
+        NSString* query = [[command arguments] objectAtIndex:1];
+        
+        // Find collection by the name handle.
+        EJDBCollection *collection = (EJDBCollection*)[collectionHandles objectForKey: name];
+        
+        NSError* error;
+        NSData* objectData = [query dataUsingEncoding:NSUTF8StringEncoding];
+        NSDictionary* jsonDict = [NSJSONSerialization JSONObjectWithData:objectData options:kNilOptions error:&error];
+        
+        CDVPluginResult* result = [CDVPluginResult
+                                   resultWithStatus:CDVCommandStatus_ERROR];
+        
+        if (collection && objectData && jsonDict) {
+            EJDBQuery *query = [[EJDBQuery alloc]initWithCollection:collection
+                                                              query:jsonDict];
+
+            NSArray* results = [query fetchObjects];
+            NSMutableDictionary *postDict = [[NSMutableDictionary alloc]init];
+            [postDict setValue:results forKey:@"results"];
+            NSData *jsonData = [NSJSONSerialization dataWithJSONObject:postDict options:0 error:nil];
+            NSString* jsonStr = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+            
+            if(!jsonData) {
+                [self error:result callbackId:callbackId];
+                return result;
+            }
+            
+            result = [CDVPluginResult
+                      resultWithStatus:CDVCommandStatus_OK
+                      messageAsString: jsonStr];
+
+            [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+        }
+        else {
             [self error:result callbackId:callbackId];
-            return result;
         }
         
-        result = [CDVPluginResult
-                  resultWithStatus:CDVCommandStatus_OK
-                  messageAsString: jsonStr];
-
-        [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+        return result;
     }
-    else {
-        [self error:result callbackId:callbackId];
-    }
-    
-    return result;
 }
 
 - (CDVPluginResult*) remove:(CDVInvokedUrlCommand*)command {
-    NSString* callbackId = [command callbackId];
-    
-    NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* uid = [[command arguments] objectAtIndex:1];
-    
-    // Find collection by the name handle.
-    EJDBCollection *collection = (EJDBCollection*)[collectionHandles objectForKey: name];
-    
-    NSError* error;
+    @synchronized(self) {
+        NSString* callbackId = [command callbackId];
+        
+        NSString* name = [[command arguments] objectAtIndex:0];
+        NSString* uid = [[command arguments] objectAtIndex:1];
+        
+        // Find collection by the name handle.
+        EJDBCollection *collection = (EJDBCollection*)[collectionHandles objectForKey: name];
+        
+        NSError* error;
 
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_ERROR];
-    
-    if (collection && [collection removeObjectWithOID:uid]) {
-        result = [CDVPluginResult
-                  resultWithStatus:CDVCommandStatus_OK];
-        [self success:result callbackId:callbackId];
+        CDVPluginResult* result = [CDVPluginResult
+                                   resultWithStatus:CDVCommandStatus_ERROR];
+        
+        if (collection && [collection removeObjectWithOID:uid]) {
+            result = [CDVPluginResult
+                      resultWithStatus:CDVCommandStatus_OK];
+            [self success:result callbackId:callbackId];
+        }
+        else {
+            [self error:result callbackId:callbackId];
+        }
+        
+        return result;
     }
-    else {
-        [self error:result callbackId:callbackId];
-    }
-    
-    return result;
 }
 
 /*
