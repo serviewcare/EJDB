6a49fee9030fea101010ec4286f8f3bd4f7ec73c
Jason Thomas <jthomas@silvervue.com>
2016-03-07 12:24:54 -0700

    Use original file names for NString extension in case it has to do with actually extending NSString because it currently isn't working.

--- src/ios/NSString+LZCompression.m
+++ src/ios/NSString+LZCompression.m
@@ -14,7 +14,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#import "LZ.h"
+#import "NSString+LZCompression.h"
 
 @interface Data : NSObject
 @property(nonatomic, readwrite) unichar val;
